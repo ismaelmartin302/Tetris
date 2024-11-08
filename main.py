e238b73c50e28164ebc041ad610e17167fcf96c0
isma <ismaelmartin302@gmail.com>
2024-11-08 19:54:17 +0100

    "Updated window configuration: changed background color, set width and height as variables, and removed resizable option."

--- main.py
+++ main.py
@@ -3,6 +3,10 @@ from tkinter import ttk
 
 window = Tk()
 window.title("Tetris")
-window.geometry("500x500")
+window.configure(bg="#242424")
+width = "500"
+height = "500"
+window.geometry(f"{width}x{height}")
 window.resizable(0,0)
+
 window.mainloop()
