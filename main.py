aa06fcc0a5bfb253f7d593b8d7a49a998715127f
isma <ismaelmartin302@gmail.com>
2024-11-08 20:32:20 +0100

    "Refactor main.py to use a class-based approach for creating the Tetris application window"
    // Se abre dos veces la ventana (?

--- main.py
+++ main.py
@@ -1,12 +1,22 @@
 from tkinter import *
 from tkinter import ttk
 
-window = Tk()
-window.title("Tetris")
-window.configure(bg="#242424")
-width = "500"
-height = "500"
-window.geometry(f"{width}x{height}")
-window.resizable(0,0)
-
-window.mainloop()
+class Application:
+    wn_title = "Tetris"
+    
+    def __init__(self, wn_width, wn_height):
+        self.wn_width = wn_width
+        self.wn_height = wn_height
+        
+    def start(name) : 
+        window = Tk()
+        window.title("Tetris")
+        window = Tk()
+        window.title("Tetris")
+        window.configure(bg="#242424")
+        window.geometry("500x500")
+        window.resizable(0,0)
+        window.mainloop()
+        
+tetris = Application("500", "500")
+tetris.start()
