1f41ec92d620bc20f4edc42343d6924d02f6f614
isma <ismaelmartin302@gmail.com>
2024-11-09 23:16:09 +0100

    "Refactored keyboardWatcher.py to use a string for logging keyboard input, and reorganized main.py to create a Tkinter application class and moved keyboard listener logic to the end of the file."

--- keyboardWatcher.py
+++ keyboardWatcher.py
@@ -1,16 +1,17 @@
 from pynput import keyboard as kb
-log_Keyboard = []
+log_Keyboard = ""
 def press(key):
+    global log_Keyboard
     match (key):
         case kb.Key.left:
             print("Left")
         case kb.Key.right:
             ("Right")
         case kb.Key.space: 
-            log_Keyboard.append(" ")
+            log_Keyboard += " "
         case kb.Key.esc:
             return False
-    log_Keyboard.append(str(key))
+    log_Keyboard += str(key)
 		
 kb.Listener(press).run()
 print((log_Keyboard))
--- main.py
+++ main.py
@@ -2,7 +2,24 @@ from tkinter import *
 from tkinter import ttk
 from pynput import keyboard as kb
 
-def press(key):
+class Application:
+    wn_title = "Tetris"
+    wn_width = 500
+    wn_height = 500
+    wn_bgColor = "#242424"
+            
+    def start(self) : 
+        window = Tk()
+        window.title(self.wn_title)
+        window.configure(bg=self.wn_bgColor)
+        window.geometry(f"{self.wn_width}x{self.wn_height}")
+        window.resizable(0,0)
+        window.mainloop()
+        
+tetris = Application()
+tetris.start()
+# Aqui es la lógica para detectar el teclado
+""" def press(key):
 	match (key):
 		case kb.Key.left:
 			print("Left")
@@ -11,20 +28,4 @@ def press(key):
 		case kb.Key.esc: 
 			return False
 
-kb.Listener(press).run()
-# class Application:
-#     wn_title = "Tetris"
-#     wn_width = 500
-#     wn_height = 500
-#     wn_bgColor = "#242424"
-            
-#     def start(self) : 
-#         window = Tk()
-#         window.title(self.wn_title)
-#         window.configure(bg=self.wn_bgColor)
-#         window.geometry(f"{self.wn_width}x{self.wn_height}")
-#         window.resizable(0,0)
-#         window.mainloop()
-        
-# tetris = Application()
-# tetris.start()
+kb.Listener(press).run() """
